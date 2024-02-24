abc2250c036ea23e2214394254a7ee3821d410dd
Patrick Weiss <patrick.weiss@wu.ac.at>
2024-02-24 20:49:19 +0000

    trim and winsorize: Add check if cut in [0,0.5]

--- R/trim.R
+++ R/trim.R
@@ -8,7 +8,7 @@
 #' @param x A numeric vector to be trimmed.
 #' @param cut The proportion of data to be trimmed from both ends of the
 #'   distribution. For example, a `cut` of 0.05 will remove the lowest and
-#'   highest 5% of the data.
+#'   highest 5% of the data. Must be inside [0,0.5].
 #'
 #' @return A numeric vector with the extreme values removed.
 #'
@@ -21,6 +21,10 @@
 #'
 #' @export
 trim <- function(x, cut) {
+  if (cut < 0 || cut > 0.5) {
+    stop("The parameter 'cut' must be inside [0,0.5].")
+  }
+
   lb <- quantile(x, cut, na.rm = TRUE)
   up <- quantile(x, 1 - cut, na.rm = TRUE)
   x <- replace(x, x > up, NA)
--- R/winsorize.R
+++ R/winsorize.R
@@ -7,7 +7,8 @@
 #' @param x A numeric vector to be winsorized.
 #' @param cut The proportion of data to be winsorized from both ends of the
 #'   distribution. For example, a `cut` of 0.05 will winsorize the lowest and
-#'   highest 5% of the data.
+#'   highest 5% of the data. Must be inside [0,0.5].
+#'
 #' @return A numeric vector with the extreme values replaced by the
 #'   corresponding quantile values.
 #'
@@ -20,6 +21,10 @@
 #'
 #' @export
 winsorize <- function(x, cut) {
+  if (cut < 0 || cut > 0.5) {
+    stop("The parameter 'cut' must be inside [0,0.5].")
+  }
+
   lb <- stats::quantile(x, cut, na.rm = TRUE)
   up <- stats::quantile(x, 1 - cut, na.rm = TRUE)
   x <- replace(x, x > up, up)
