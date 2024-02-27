ec87229826272a15122d2ec444b05176a9aabfe4
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2024-02-27 11:50:18 +0100

    Added download_data_wrds_fisd() yet untested

--- R/download_data.R
+++ R/download_data.R
@@ -93,7 +93,9 @@ list_supported_types_wrds <- function() {
     "wrds_crsp_monthly", "crsp.msf, crsp.msenames, crsp.msedelist",
     "wrds_crsp_daily", "crsp.dsf, crsp.msenames, crsp.msedelist",
     "wrds_compustat_annual", "comp.funda",
-    "wrds_ccm_links", "crsp.ccmxpf_linktable"
+    "wrds_ccm_links", "crsp.ccmxpf_linktable",
+    "wrds_fisd", "fisd.fisd_mergedissue, fisd.fisd_mergedissuer",
+    "wrds_trace", "trace.trace_enhanced"
   ) |>
     mutate(domain = "WRDS")
 }
@@ -831,6 +833,87 @@ download_data_wrds_ccm_links <- function(
   ccm_links
 }
 
+#' Download Filtered FISD Data from WRDS
+#'
+#' Establishes a connection to the WRDS database to download a filtered subset of the FISD (Fixed Income Securities Database).
+#' The function filters the `fisd_mergedissue` and `fisd_mergedissuer` tables based on several criteria related to the securities,
+#' such as security level, bond type, coupon type, and others, focusing on specific attributes that denote the nature of the securities.
+#' It finally returns a data frame with selected fields from the `fisd_mergedissue` table, after joining it with issuer information
+#' from the `fisd_mergedissuer` table for issuers domiciled in the USA.
+#'
+#' @return A data frame containing a subset of FISD data with fields related to the bond's characteristics and issuer information.
+#'   This includes complete CUSIP, maturity, offering amount, offering date, dated date, interest frequency, coupon,
+#'   last interest date, issue ID, issuer ID, SIC code of the issuer.
+#'
+#' @examples
+#' download_data_wrds_fisd()
+#'
+#' @import dplyr
+#'
+#' @export
+download_data_wrds_fisd <- function() {
+
+  con <- get_wrds_connection()
+
+  check_if_package_installed("dbplyr", "fisd_mergedissue")
+
+  in_schema <- getNamespace("dbplyr")$in_schema
+
+  fisd_mergedissue_db <- tbl(wrds, in_schema("fisd", "fisd_mergedissue"))
+
+  fisd <- fisd_mergedissue_db |>
+    filter(
+      security_level == "SEN",
+      slob == "N" | is.na(slob),
+      is.na(security_pledge),
+      asset_backed == "N" | is.na(asset_backed),
+      defeased == "N" | is.na(defeased),
+      is.na(defeased_date),
+      bond_type %in% c("CDEB", "CMTN", "CMTZ", "CZ", "USBN"),
+      pay_in_kind != "Y" | is.na(pay_in_kind),
+      is.na(pay_in_kind_exp_date),
+      yankee == "N" | is.na(yankee),
+      canadian == "N" | is.na(canadian),
+      foreign_currency == "N",
+      coupon_type %in% c("F", "Z"),
+      is.na(fix_frequency),
+      coupon_change_indicator == "N",
+      interest_frequency %in% c("0", "1","2", "4", "12"),
+      rule_144a == "N",
+      private_placement == "N" | is.na(private_placement),
+      defaulted == "N",
+      is.na(filing_date),
+      is.na(settlement),
+      convertible == "N",
+      is.na(exchange),
+      putable == "N" | is.na(putable),
+      unit_deal == "N" | is.na(unit_deal),
+      exchangeable == "N" | is.na(exchangeable),
+      perpetual == "N",
+      preferred_security == "N" | is.na(preferred_security)
+    ) |>
+    select(
+      complete_cusip, maturity,
+      offering_amt, offering_date,
+      dated_date,
+      interest_frequency, coupon,
+      last_interest_date,
+      issue_id, issuer_id
+    ) |>
+    collect()
+
+  fisd_mergedissuer_db <- tbl(wrds, in_schema("fisd", "fisd_mergedissuer"))
+
+  fisd_issuer <- fisd_mergedissuer_db |>
+    filter(country_domicile == "USA") |>
+    select(issuer_id, sic_code) |>
+    collect()
+
+  fisd <- fisd |>
+    inner_join(fisd_issuer, join_by(issuer_id))
+
+  fisd
+}
 
 # Set global variables ------------------------------------------------
 utils::globalVariables(
@@ -849,7 +932,21 @@ utils::globalVariables(
     "dlstcd", "dlstdt", "exchcd", "gvkey", "indfmt", "inv", "itcb", "linkdt", "linkenddt",
     "lpermno", "lt", "mktcap", "namedt", "nameendt", "oancf", "permno", "pstk", "pstkl", "pstkrv", "ret",
     "ret_adj", "ret_excess", "risk_free", "sale", "shrcd", "shrout", "siccd", "txdb", "txditc", "xint",
-    "xsga",
-    "ret_excess"
+    "xsga","ret_excess"
+  )
+)
+
+utils::globalVariables(
+  c(
+    "issuer_id", "sic_code", "country_domicile",
+    "complete_cusip", "maturity", "offering_amt", "offering_date",
+    "dated_date", "interest_frequency", "coupon", "last_interest_date",
+    "issue_id", "security_level", "slob", "security_pledge", "asset_backed",
+    "defeased", "defeased_date", "bond_type", "pay_in_kind","pay_in_kind_exp_date",
+    "yankee", "canadian", "foreign_currency", "coupon_type", "fix_frequency",
+    "coupon_change_indicator", "interest_frequency", "rule_144a",
+    "private_placement", "defaulted", "filing_date", "settlement",
+    "convertible", "exchange", "putable", "unit_deal", "exchangeable",
+    "perpetual", "preferred_security"
   )
 )
