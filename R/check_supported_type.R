361b2aa5dfd46f0bc2422bb4f1dcc620450a68e5
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2024-07-03 10:24:49 +0200

    Updated check_supported_types()

--- R/check_supported_type.R
+++ R/check_supported_type.R
@@ -12,8 +12,8 @@
 #'   unsupported.
 #'
 check_supported_type <- function(type) {
-  supported_types <- list_supported_types()
-  if (!any(type %in% supported_types$type)) {
+  supported_types <- list_supported_types(as_vector = TRUE)
+  if (!any(type %in% supported_types)) {
     stop("Unsupported type specified. Call the function list_supported_types() to get all supported types.")
   }
 }
