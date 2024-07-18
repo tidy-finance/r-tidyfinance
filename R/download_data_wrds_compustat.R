814145a529e163b695f96293b41801d76924d7ef
Patrick Weiss <patrick.weiss@wu.ac.at>
2024-07-18 10:00:02 +0000

    change group_by and arrange order

--- R/download_data_wrds_compustat.R
+++ R/download_data_wrds_compustat.R
@@ -128,8 +128,8 @@ download_data_wrds_compustat <- function(
       filter(datadate == max(datadate)) |>
       slice_head(n = 1) |>
       ungroup() |>
-      arrange(gvkey, date, rdq) |>
       group_by(gvkey, date) |>
+      arrange(gvkey, date, rdq) |>
       slice_head(n = 1) |>
       ungroup() |>
       filter(if_else(is.na(rdq), TRUE, date < rdq))
