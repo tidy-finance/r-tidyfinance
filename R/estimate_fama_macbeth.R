b3d880ab6d80f5fbb5d7926e90fc95d883a9d879
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2024-08-10 11:27:17 +0200

    Added roxygen coments to estimate_fama_macbeth()

--- R/estimate_fama_macbeth.R
+++ R/estimate_fama_macbeth.R
@@ -1,30 +1,66 @@
+#' Estimate Fama-MacBeth Regressions
+#'
+#' This function estimates Fama-MacBeth regressions by first running cross-sectional regressions
+#' for each time period and then aggregating the results over time to obtain average risk premia
+#' and corresponding t-statistics.
+#'
+#' @param data A data frame containing the data for the regression. It must include a `date` column
+#'   representing the time periods and the variables specified in the `model`.
+#' @param model A formula representing the regression model to be estimated in each cross-section.
+#' @param vcov A character string indicating the type of standard errors to compute. Options are `"iid"`
+#'   for independent and identically distributed errors or `"newey-west"` for Newey-West standard errors.
+#'   Default is `"newey-west"`.
+#'
+#' @return A data frame with the estimated risk premiums, the number of observations, standard errors,
+#'   and t-statistics for each factor in the model.
+#'
+#' @export
+#'
+#' @examples
+#' data <- tibble(
+#'   date = rep(seq.Date(from = as.Date("2020-01-01"), to = as.Date("2020-12-01"), by = "month"), each = 50),
+#'   permno = rep(1:50, times = 12),
+#'   ret_excess = rnorm(600, 0, 0.1),
+#'   beta = rnorm(600, 1, 0.2),
+#'   bm = rnorm(600, 0.5, 0.1),
+#'   log_mktcap = rnorm(600, 10, 1)
+#' )
+#'
+#' estimate_fama_macbeth(data, "ret_excess ~ beta + bm + log_mktcap")
+#' estimate_fama_macbeth(data, "ret_excess ~ beta + bm + log_mktcap", vcov = "iid")
+#'
 estimate_fama_macbeth <- function(data, model, vcov = "newey-west") {
 
-  # TODO: add check for vcov and model (from estimate_model or rely on its checks?)
+  # Check that vcov is one of the allowed options
+  if (!vcov %in% c("iid", "newey-west")) {
+    cli::cli_abort("{.arg vcov} must be either 'iid' or 'newey-west'.")
+  }
 
   # Cross-sectional regressions
   cross_sections <- data |>
-    nest(data = -date) |>
-    mutate(estimates = map(data, ~ estimate_model(., model))) |>
-    unnest(estimates) |>
+    tidyr::nest(data = -date) |>
+    mutate(estimates = purrr::map(data, ~ estimate_model(., model))) |>
+    tidyr::unnest(estimates) |>
     select(-data) |>
-    pivot_longer(-date)
+    tidyr::pivot_longer(-date)
 
+  # Function to compute the standard error based on the specified vcov
   compute_standard_error <- function(model, vcov) {
     if (vcov == "iid") {
       sqrt(stats::vcov(model)[1, 1])
     } else if (vcov == "newey-west") {
+      check_if_package_installed("sandwich")
       sqrt(sandwich::NeweyWest(model))
     }
   }
 
   # Time-series aggregations
   aggregations <- cross_sections |>
-    nest(data = c(date, value)) |>
-    mutate(model = map(data, ~ lm("value ~ 1", data = .)),
-           risk_premium = map_dbl(model, ~ .$coefficients),
-           n = map_dbl(data, nrow),
-           standard_error = map_dbl(model, ~ compute_standard_error(., vcov))
+    tidyr::nest(data = c(date, value)) |>
+    mutate(model = purrr::map(data, ~ lm("value ~ 1", data = .)),
+           risk_premium = purrr::map_dbl(model, ~ .$coefficients),
+           n = purrr::map_dbl(data, nrow),
+           standard_error = purrr::map_dbl(model, ~ compute_standard_error(., vcov))
     ) |>
     mutate(t_statistic = case_when(
       vcov == "iid" ~ risk_premium / standard_error * sqrt(n),
@@ -35,15 +71,3 @@ estimate_fama_macbeth <- function(data, model, vcov = "newey-west") {
   aggregations
 
 }
-
-data <- tibble(
-  date = rep(seq.Date(from = as.Date("2020-01-01"), to = as.Date("2020-12-01"), by = "month"), each = 50),
-  permno = rep(1:50, times = 12),
-  ret_excess = rnorm(600, 0, 0.1),
-  beta = rnorm(600, 1, 0.2),
-  bm = rnorm(600, 0.5, 0.1),
-  log_mktcap = rnorm(600, 10, 1)
-)
-
-estimate_fama_macbeth(data, "ret_excess ~ beta + bm + log_mktcap")
-estimate_fama_macbeth(data, "ret_excess ~ beta + bm + log_mktcap", vcov = "iid")
