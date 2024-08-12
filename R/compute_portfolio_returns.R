8c24ccc5fb366431918062241de014b214310ab3
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2024-08-12 09:15:59 -0700

    Implemented annual rebalancing in compute_portfolio_returns and deleted old file

--- R/compute_portfolio_returns.R
+++ R/compute_portfolio_returns.R
@@ -1,337 +1,209 @@
-#' Portfolio sort: Annual, single sort
-#'
-#' Perform portfolio sort # TODO: Expand when parameters are settled.
-#'
-#' @param sorting_data Some dataset... # TODO: Expand when parameters are settled.
-#'
-#' @return A tibble with returns per portfolio.
-#'
-#' @examples
-#' \donttest{
-#'   compute_portfolio_return_annual_single(crsp_monthly, "shrout") # TODO: Is there a more interesting example with the base data?
-#' }
-#'
-#' @export
-compute_portfolio_return_annual_single <- function(
+compute_portfolio_returns <- function(
     sorting_data,
-    sorting_variable,
-    breakpoints_main = 5, # TODO: Do we want to support breakpoints using percentiles in the function assign_portfolios?
-    breakpoints_exchanges = "NYSE",
-    weighting_scheme = "VW"
+    sorting_variables,
+    sorting_method,
+    rebalancing_frequency = "monthly",
+    n_portfolios = NULL, # must be a vector
+    percentiles = NULL, # must be a list with length == length(sorting_variables)
+    breakpoint_exchanges = NULL
 ) {
-  # Create portfolio assignment
-  portfolio_data <- sorting_data |> # TODO: In principle, we can make this replaceable and allow passing a tibble with permno, date, portfolio?!
-    filter(month(date) == 7) |>
-    group_by(date) |>
-    mutate(
-      portfolio = assign_portfolios(
-        sorting_data = pick(all_of(sorting_variable), exchange),
-        sorting_variable = sorting_variable,
-        breakpoints_number = breakpoints_main,
-        breakpoints_exchanges = breakpoints_exchanges
-      )) |>
-    ungroup() |>
-    select(permno, date, portfolio) |>
-    drop_na()
 
-  # Merge and compute returns
-  sorting_data |>
-    left_join(portfolio_data |>
-                mutate(lower_bound = date,
-                       upper_bound = date + months(12)) |>
-                select(-date),
-              join_by(permno, closest(date >= lower_bound), date < upper_bound),
-              relationship = "many-to-one") |>
-    drop_na(portfolio) |>
-    group_by(date, portfolio) |>
-    summarize(ret_excess = if_else(weighting_scheme == "VW",
-                                   weighted.mean(ret_excess, mktcap_lag),
-                                   mean(ret_excess)),
-              .groups = "drop")
-}
+  # TODO: add checks
+  # sorting_variables, sorting_method and (n_portfolios or percentiles) must be given
+  # if exchanges are provided, check whether the data contains the column
+  # check whether sorting_data has sorting_variable and mktcap_lag column
 
+  # TODO: pass percentiles instead of n_portfolios
 
-#' Portfolio sort: Annual, independent double sort
-#'
-#' Perform portfolio sort
-#'
-#' @param sorting_data Some dataset...
-#'
-#' @return A tibble with returns per portfolio.
-#'
-#' @examples
-#' \donttest{
-#'   compute_portfolio_return_annual_double_independent(crsp_monthly, "shrout")
-#' }
-#'
-#' @export
-compute_portfolio_return_annual_double_independent <- function(
-    sorting_data,
-    sorting_variable,
-    breakpoints_main = 5,
-    breakpoints_secondary = 2,
-    breakpoints_exchanges = "NYSE",
-    weighting_scheme = "VW"
-) {
-  # Create portfolio assignment
-  portfolio_data <- sorting_data |>
-    filter(month(date) == 7) |>
-    group_by(date) |>
-    mutate(
-      portfolio_main = assign_portfolios(
-        sorting_data = pick(all_of(sorting_variable), exchange),
-        sorting_variable = sorting_variable,
-        breakpoints_number = breakpoints_main,
-        breakpoints_exchanges = breakpoints_exchanges
-      ),
-      portfolio_secondary = assign_portfolios(
-        sorting_data = pick(mktcap_lag, exchange),
-        sorting_variable = "mktcap_lag",
-        breakpoints_number = breakpoints_secondary,
-        breakpoints_exchanges = breakpoints_exchanges
-      )) |>
-    ungroup() |>
-    drop_na(portfolio_main, portfolio_secondary) |>
-    mutate(portfolio = paste0(portfolio_main, "-", portfolio_secondary)) |>
-    select(permno, date, portfolio_main, portfolio)
+  if (sorting_method == "univariate") {
 
-  # Merge and compute returns
-  sorting_data |>
-    left_join(portfolio_data |>
-                mutate(lower_bound = date,
-                       upper_bound = date + months(12)) |>
-                select(-date),
-              join_by(permno, closest(date >= lower_bound), date < upper_bound),
-              relationship = "many-to-one") |>
-    drop_na(portfolio) |>
-    group_by(date, portfolio) |>
-    summarize(ret_excess = if_else(weighting_scheme == "VW",
-                                   weighted.mean(ret_excess, mktcap_lag),
-                                   mean(ret_excess)),
-              portfolio_main = unique(portfolio_main),
-              .groups = "drop") |>
-    group_by(date, portfolio_main) |>
-    summarize(ret_excess = mean(ret_excess),
-              .groups = "drop") |>
-    rename(portfolio = portfolio_main)
-}
+    if (length(sorting_variables) > 1) {
+      cli::cli_abort("Only provide one sorting variable for univariate sorts.")
+    }
+    if (length(n_portfolios) > 1) {
+      cli::cli_abort("Only provide one number of portfolios for univariate sorts.")
+    }
 
+    if (rebalancing_frequency == "monthly") {
+      portfolio_returns <- sorting_data |>
+        group_by(date) |>
+        mutate(
+          portfolio = assign_portfolio(
+            data = pick(everything()),
+            sorting_variable = sorting_variables[1],
+            n_portfolios = n_portfolios[1],
+            breakpoint_exchanges = breakpoint_exchanges
+          )
+        )
+    } else if (rebalancing_frequency == "annual") {
+      portfolio_data <- sorting_data |>
+        filter(month(date) == 7) |>
+        group_by(date) |>
+        mutate(
+          portfolio = assign_portfolio(
+            data = pick(everything()),
+            sorting_variable = sorting_variables[1],
+            n_portfolios = n_portfolios[1],
+            breakpoint_exchanges = breakpoint_exchanges
+          )) |>
+        ungroup() |>
+        select(permno, date, portfolio)
 
-#' Portfolio sort: Annual, dependent double sort
-#'
-#' Perform portfolio sort
-#'
-#' @param sorting_data Some dataset...
-#'
-#' @return A tibble with returns per portfolio.
-#'
-#' @examples
-#' \donttest{
-#'   compute_portfolio_return_annual_double_dependent(crsp_monthly, "shrout")
-#' }
-#'
-#' @export
-compute_portfolio_return_annual_double_dependent <- function(
-    sorting_data,
-    sorting_variable,
-    breakpoints_main = 5,
-    breakpoints_secondary = 2,
-    breakpoints_exchanges = "NYSE",
-    weighting_scheme = "VW"
-) {
-  # Create portfolio assignment
-  portfolio_data <- sorting_data |>
-    filter(month(date) == 7) |>
-    group_by(date) |>
-    mutate(
-      portfolio_secondary = assign_portfolios(
-        sorting_data = pick(mktcap_lag, exchange),
-        sorting_variable = "mktcap_lag",
-        breakpoints_number = breakpoints_secondary,
-        breakpoints_exchanges = breakpoints_exchanges
-      )) |>
-    ungroup() |>
-    drop_na(portfolio_secondary) |>
-    group_by(date, portfolio_secondary) |>
-    mutate(
-      portfolio_main = assign_portfolios(
-        sorting_data = pick(all_of(sorting_variable), exchange),
-        sorting_variable = sorting_variable,
-        breakpoints_number = breakpoints_main,
-        breakpoints_exchanges = breakpoints_exchanges
-      )) |>
-    ungroup() |>
-    drop_na(portfolio_main) |>
-    mutate(portfolio = paste0(portfolio_main, "-", portfolio_secondary)) |>
-    select(permno, date, portfolio_main, portfolio)
+      portfolio_returns <- sorting_data |>
+        left_join(portfolio_data |>
+                    mutate(lower_bound = date,
+                           upper_bound = date + months(12)) |>
+                    select(-date),
+                  join_by(permno, closest(date >= lower_bound), date < upper_bound),
+                  relationship = "many-to-one")
+    }
 
-  # Merge and compute returns
-  sorting_data |>
-    left_join(portfolio_data |>
-                mutate(lower_bound = date,
-                       upper_bound = date + months(12)) |>
-                select(-date),
-              join_by(permno, closest(date >= lower_bound), date < upper_bound),
-              relationship = "many-to-one") |>
-    drop_na(portfolio) |>
-    group_by(date, portfolio) |>
-    summarize(ret_excess = if_else(weighting_scheme == "VW",
-                                   weighted.mean(ret_excess, mktcap_lag),
-                                   mean(ret_excess)),
-              portfolio_main = unique(portfolio_main),
-              .groups = "drop") |>
-    group_by(date, portfolio_main) |>
-    summarize(ret_excess = mean(ret_excess),
-              .groups = "drop") |>
-    rename(portfolio = portfolio_main)
-}
+    portfolio_returns <- portfolio_returns |>
+      group_by(portfolio, date) |>
+      summarize(
+        ret_excess_vw = stats::weighted.mean(ret_excess, mktcap_lag),
+        ret_excess_ew = mean(ret_excess),
+        .groups = "drop"
+      )
+  }
 
+  if (sorting_method == "bivariate-dependent") {
+    if (length(sorting_variables) != 2) {
+      cli::cli_abort("Provide two sorting variables for bivariate sorts.")
+    }
+    if (length(n_portfolios) != 2) {
+      cli::cli_abort("Provide two numbers of portfolios for bivariate sorts.")
+    }
 
-#' Portfolio sort: Monthly, single sort
-#'
-#' Perform portfolio sort
-#'
-#' @param sorting_data Some dataset...
-#'
-#' @return A tibble with returns per portfolio.
-#'
-#' @examples
-#' \donttest{
-#'   compute_portfolio_return_monthly_single(crsp_monthly, "shrout")
-#' }
-#'
-#' @export
-compute_portfolio_return_monthly_single <- function(
-    sorting_data,
-    sorting_variable,
-    breakpoints_main = 5,
-    breakpoints_exchanges = "NYSE",
-    weighting_scheme = "VW"
-) {
-  sorting_data |>
-    group_by(date) |>
-    mutate(
-      portfolio = assign_portfolios(
-        sorting_data = pick(all_of(sorting_variable), exchange),
-        sorting_variable = sorting_variable,
-        breakpoints_number = breakpoints_main,
-        breakpoints_exchanges = breakpoints_exchanges
-      )) |>
-    ungroup() |>
-    drop_na(portfolio) |>
-    group_by(date, portfolio) |>
-    summarize(ret_excess = if_else(weighting_scheme == "VW",
-                                   weighted.mean(ret_excess, mktcap_lag),
-                                   mean(ret_excess)),
-              .groups = "drop")
-}
+    if (rebalancing_frequency == "monthly") {
+      portfolio_returns <- sorting_data |>
+        group_by(date) |>
+        mutate(
+          portfolio_secondary = assign_portfolio(
+            pick(everything()),
+            sorting_variable = sorting_variables[1],
+            n_portfolios = n_portfolios[1],
+            breakpoint_exchanges = breakpoint_exchanges
+          )) |>
+        ungroup() |>
+        group_by(date, portfolio_secondary) |>
+        mutate(
+          portfolio_main = assign_portfolio(
+            pick(everything()),
+            sorting_variable = sorting_variables[2],
+            n_portfolios = n_portfolios[2],
+            breakpoint_exchanges = breakpoint_exchanges
+          )) |>
+        ungroup()
 
+    } else if (rebalancing_frequency == "annual") {
+      portfolio_data <- sorting_data |>
+        filter(month(date) == 7) |>
+        group_by(date) |>
+        mutate(
+          portfolio_secondary = assign_portfolio(
+            pick(everything()),
+            sorting_variable = sorting_variables[1],
+            n_portfolios = n_portfolios[1],
+            breakpoint_exchanges = breakpoint_exchanges
+          )) |>
+        ungroup() |>
+        group_by(date, portfolio_secondary) |>
+        mutate(
+          portfolio_main = assign_portfolio(
+            pick(everything()),
+            sorting_variable = sorting_variables[2],
+            n_portfolios = n_portfolios[2],
+            breakpoint_exchanges = breakpoint_exchanges
+          )) |>
+        ungroup() |>
+        select(permno, date, portfolio_main, portfolio_secondary)
 
-#' Portfolio sort: Monthly, independent double sort
-#'
-#' Perform portfolio sort
-#'
-#' @param sorting_data Some dataset...
-#'
-#' @return A tibble with returns per portfolio.
-#'
-#' @examples
-#' \donttest{
-#'   compute_portfolio_return_monthly_double_independent(crsp_monthly, "shrout")
-#' }
-#'
-#' @export
-compute_portfolio_return_monthly_double_independent <- function(
-    sorting_data,
-    sorting_variable,
-    breakpoints_main = 5,
-    breakpoints_secondary = 2,
-    breakpoints_exchanges = "NYSE",
-    weighting_scheme = "VW"
-) {
-  sorting_data |>
-    group_by(date) |>
-    mutate(
-      portfolio_main = assign_portfolios(
-        sorting_data = pick(all_of(sorting_variable), exchange),
-        sorting_variable = sorting_variable,
-        breakpoints_number = breakpoints_main,
-        breakpoints_exchanges = breakpoints_exchanges
-      ),
-      portfolio_secondary = assign_portfolios(
-        sorting_data = pick(mktcap_lag, exchange),
-        sorting_variable = "mktcap_lag",
-        breakpoints_number = breakpoints_secondary,
-        breakpoints_exchanges = breakpoints_exchanges
-      )) |>
-    ungroup() |>
-    drop_na(portfolio_main, portfolio_secondary) |>
-    mutate(portfolio = paste0(portfolio_main, "-", portfolio_secondary)) |>
-    group_by(date, portfolio) |>
-    summarize(ret_excess = if_else(weighting_scheme == "VW",
-                                   weighted.mean(ret_excess, mktcap_lag),
-                                   mean(ret_excess)),
-              portfolio_main = unique(portfolio_main),
-              .groups = "drop") |>
-    group_by(date, portfolio_main) |>
-    summarize(ret_excess = mean(ret_excess),
-              .groups = "drop") |>
-    rename(portfolio = portfolio_main)
-}
+      portfolio_returns <- sorting_data |>
+        left_join(portfolio_data |>
+                    mutate(lower_bound = date,
+                           upper_bound = date + months(12)) |>
+                    select(-date),
+                  join_by(permno, closest(date >= lower_bound), date < upper_bound),
+                  relationship = "many-to-one")
+    }
+    portfolio_returns <- portfolio_returns  |>
+      group_by(portfolio_main, portfolio_secondary, date) |>
+      summarize(
+        ret_excess_vw = stats::weighted.mean(ret_excess, mktcap_lag),
+        ret_excess_ew = mean(ret_excess),
+        .groups = "drop"
+      ) |>
+      group_by(portfolio = portfolio_main, date) |>
+      summarize(across(c(ret_excess_vw, ret_excess_ew), mean),
+                .groups = "drop")
+  }
 
+  if (sorting_method == "bivariate-independent") {
+    if (length(sorting_variables) != 2) {
+      cli::cli_abort("Provide two sorting variables for bivariate sorts.")
+    }
+    if (length(n_portfolios) != 2) {
+      cli::cli_abort("Provide two numbers of portfolios for bivariate sorts.")
+    }
 
-#' Portfolio sort: Monthly, dependent double sort
-#'
-#' Perform portfolio sort
-#'
-#' @param sorting_data Some dataset...
-#'
-#' @return A tibble with returns per portfolio.
-#'
-#' @examples
-#' \donttest{
-#'   compute_portfolio_return_monthly_double_dependent(crsp_monthly, "shrout")
-#' }
-#'
-#' @export
-compute_portfolio_return_monthly_double_dependent <- function(
-    sorting_data,
-    sorting_variable,
-    breakpoints_main = 5,
-    breakpoints_secondary = 2,
-    breakpoints_exchanges = "NYSE",
-    weighting_scheme = "VW"
-) {
-  sorting_data |>
-    group_by(date) |>
-    mutate(
-      portfolio_secondary = assign_portfolios(
-        sorting_data = pick(mktcap_lag, exchange),
-        sorting_variable = "mktcap_lag",
-        breakpoints_number = breakpoints_secondary,
-        breakpoints_exchanges = breakpoints_exchanges
-      )) |>
-    ungroup() |>
-    drop_na(portfolio_secondary) |>
-    group_by(date, portfolio_secondary) |>
-    mutate(
-      portfolio_main = assign_portfolios(
-        sorting_data = pick(all_of(sorting_variable), exchange),
-        sorting_variable = sorting_variable,
-        breakpoints_number = breakpoints_main,
-        breakpoints_exchanges = breakpoints_exchanges
-      )) |>
-    ungroup() |>
-    drop_na(portfolio_main) |>
-    mutate(portfolio = paste0(portfolio_main, "-", portfolio_secondary)) |>
-    group_by(date, portfolio) |>
-    summarize(ret_excess = if_else(weighting_scheme == "VW",
-                                   weighted.mean(ret_excess, mktcap_lag),
-                                   mean(ret_excess)),
-              portfolio_main = unique(portfolio_main),
-              .groups = "drop") |>
-    group_by(date, portfolio_main) |>
-    summarize(ret_excess = mean(ret_excess),
-              .groups = "drop") |>
-    rename(portfolio = portfolio_main)
+    if (rebalancing_frequency == "monthly") {
+      portfolio_returns <- sorting_data |>
+        group_by(date) |>
+        mutate(
+          portfolio_secondary = assign_portfolio(
+            pick(everything()),
+            sorting_variable = sorting_variables[1],
+            n_portfolios = n_portfolios[1],
+            breakpoint_exchanges = breakpoint_exchanges
+          ),
+          portfolio_main = assign_portfolio(
+            pick(everything()),
+            sorting_variable = sorting_variables[2],
+            n_portfolios = n_portfolios[2],
+            breakpoint_exchanges = breakpoint_exchanges
+          )) |>
+        ungroup()
+
+    } else if (rebalancing_frequency == "annual") {
+      portfolio_data <- sorting_data |>
+        filter(month(date) == 7) |>
+        group_by(date) |>
+        mutate(
+          portfolio_secondary = assign_portfolio(
+            pick(everything()),
+            sorting_variable = sorting_variables[1],
+            n_portfolios = n_portfolios[1],
+            breakpoint_exchanges = breakpoint_exchanges
+          ),
+          portfolio_main = assign_portfolio(
+            pick(everything()),
+            sorting_variable = sorting_variables[2],
+            n_portfolios = n_portfolios[2],
+            breakpoint_exchanges = breakpoint_exchanges
+          )) |>
+        ungroup() |>
+        select(permno, date, portfolio_main, portfolio)
+
+      portfolio_returns <- sorting_data |>
+        left_join(portfolio_data |>
+                    mutate(lower_bound = date,
+                           upper_bound = date + months(12)) |>
+                    select(-date),
+                  join_by(permno, closest(date >= lower_bound), date < upper_bound),
+                  relationship = "many-to-one")
+    }
+    portfolio_returns <- portfolio_returns  |>
+      group_by(portfolio_main, portfolio_secondary, date) |>
+      summarize(
+        ret_excess_vw = stats::weighted.mean(ret_excess, mktcap_lag),
+        ret_excess_ew = mean(ret_excess),
+        .groups = "drop"
+      ) |>
+      group_by(portfolio = portfolio_main, date) |>
+      summarize(across(c(ret_excess_vw, ret_excess_ew), mean),
+                .groups = "drop")
+  }
+
+  portfolio_returns
 }
