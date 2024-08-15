b0b6d9d538b6e03be9bb1f883c328de08fbaaa75
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2024-08-15 08:46:50 -0700

    Updated check_installed() reason

--- R/estimate_betas.R
+++ R/estimate_betas.R
@@ -108,7 +108,7 @@ estimate_betas <- function(
   }
 
   if (use_furrr) {
-    rlang::check_installed("furrr", reason = "To use furrr::future_map")
+    rlang::check_installed("furrr", reason = "To use furrr::future_map in estimate_betas()")
 
     betas <- data |>
       tidyr::nest(data = -permno) |>
