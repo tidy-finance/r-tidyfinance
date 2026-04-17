da49b41b2b9589b1b7ade67c66761c2d30c98df0
Patrick Weiss <patrick.weiss@wu.ac.at>
2026-04-17 17:36:24 +0200

    Remove code to prepare supported types for FF

--- R/list_supported_types.R
+++ R/list_supported_types.R
@@ -13,28 +13,8 @@
 #' @family utility functions
 list_supported_types_ff <- function() {
   # nolint start
-  # data_sets_raw <- frenchdata::get_french_data_list()$files_list
-  #
-  # data_sets_types <- data_sets_raw |>
-  #   mutate(type = gsub("Fama/French ", "", name),
-  #          type = gsub(" Factors", "", type),
-  #          type = gsub("Portfolios Formed on ", "", type),
-  #          type = gsub(" \\(Mom\\)", "", type),
-  #          type = gsub("[[:punct:]]", "", type),
-  #          type = gsub(" ", "_", type),
-  #          type = tolower(type),
-  #          type = if_else(!grepl("weekly|daily", type), paste0(type, "_monthly"), type),
-  #          type = gsub("booktomarket", "bm", type),
-  #          type = gsub("operating_profitability", "op", type),
-  #          type = gsub("ex_dividend", "exdividend", type),
-  #          type = gsub("investment", "inv", type),
-  #          type = gsub("\t", "_", type),
-  #          type = paste0("factors_ff_", type)) |>
-  #   select(type, dataset_name = name)
-  #
-  # datapasta::dp_set_max_rows(300)
-  # datapasta::tribble_paste(data_sets_types)
-
+  # To update this list, run data-raw/list_supported_types_ff.R and replace
+  # the tribble() call below with the output of datapasta::tribble_paste().
   tribble(
     ~type                                                                                                         ,
     ~dataset_name                                                                                                 ,
