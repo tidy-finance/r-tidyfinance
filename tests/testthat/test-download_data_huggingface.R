a9b742aa4f7b0f4833633d53a437b33cbbfb64d1
Patrick Weiss <patrick.weiss@wu.ac.at>
2026-05-07 18:02:59 +0200

    Fix error

--- tests/testthat/test-download_data_huggingface.R
+++ tests/testthat/test-download_data_huggingface.R
@@ -1,13 +1,13 @@
 # Helpers ---------------------------------------------------------------
 
 make_grid_parquet_file <- function(rows) {
-  tmp <- withr::local_tempfile(fileext = ".parquet")
+  tmp <- tempfile(fileext = ".parquet")
   arrow::write_parquet(rows, tmp)
   tibble::tibble(path = "grid/part.parquet", size = file.size(tmp), url = tmp)
 }
 
 make_returns_parquet_file <- function(rows, sv, sv_lag) {
-  tmp <- withr::local_tempfile(fileext = ".parquet")
+  tmp <- tempfile(fileext = ".parquet")
   arrow::write_parquet(rows, tmp)
   tibble::tibble(
     path = paste0(
